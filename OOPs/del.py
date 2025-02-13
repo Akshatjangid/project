ced5c1572928109e28f7e0fb096da2b6d72fb4f7
Akshatjangid <akshatjangid9@gmail.com>
2025-02-13 10:16:13 +0530

    Delete OOPs directory

--- OOPs/del.py
+++
@@ -1,9 +0,0 @@
-class student:
-    def __init__(self,name,roll_no):
-        self.name=name
-        self.roll_no=roll_no
-    
-s1=student("Akshat",11)
-print(s1.name)
-del s1.roll_no
-print(s1.roll_no)
